1cabd0ae8401c58a41115ab07472c673c13c4cb6
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-07-26 14:08:30 +0800

    Update h1.py

--- h1.py
+++ h1.py
@@ -1,58 +1,15 @@
-#-*- coding:utf-8 -*-
-import sys, os, BaseHTTPServer
-import BaseHTTPServer
+# -*- coding: utf-8 -*-
+# filename: main.py
+import web
 
-class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    '''处理请求并返回页面'''
+urls = (
+    '/wx', 'Handle',
+)
 
-    # 页面模板
-    Page = '''\
-    <html>
-    <body>
-    <table>
-    <tr>  <td>Header</td>         <td>Value</td>          </tr>
-    <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
-    <tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
-    <tr>  <td>Client port</td>    <td>{client_port}</td> </tr>
-    <tr>  <td>Command</td>        <td>{command}</td>      </tr>
-    <tr>  <td>Path</td>           <td>{path}</td>         </tr>
-    </table>
-    </body>
-    </html>
-    '''
-
-    # 处理一个GET请求
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(self.Page)))
-        self.end_headers()
-        self.wfile.write(self.Page)
-
-
-
-
-    def create_page(self):
-        values = {
-            'date_time'   : self.date_time_string(),
-            'client_host' : self.client_address[0],
-            'client_port' : self.client_address[1],
-            'command'     : self.command,
-            'path'        : self.path
-        }
-        page = self.Page.format(**values)
-        return page
-    def send_content(self, page):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.send_header("Content-Length", str(len(page)))
-        self.end_headers()
-        self.wfile.write(page)
-
-
-#----------------------------------------------------------------------
+class Handle(object):
+    def GET(self):
+        return "hello, this is 88888 a test"
 
 if __name__ == '__main__':
-    serverAddress = ('', 8080)
-    server = BaseHTTPServer.HTTPServer(serverAddress, RequestHandler)
-    server.serve_forever()
+    app = web.application(urls, globals())
+    app.run()

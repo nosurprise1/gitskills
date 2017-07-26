2bbe8af1b8adc38746727cd9ff26a7a61e246b77
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-07-26 21:17:00 +0800

    Update h.py

--- h.py
+++ h.py
@@ -1,37 +1,12 @@
-
-server {
-        listen 80;
-        server_name new.vtao.net;
-        access_log /var/log/nginx/new.vtao.net.log;
-        location / {
-                include fastcgi_params;
-                fastcgi_pass unix:/tmp/py-fcgi.sock;
-        }
-        location /static/ {
-                root /var/webpy;
-                if (-f $request_filename){
-                        rewrite ^/static/(.*)$ /static/$1 break;
-                }
-        }
-}
- 
- 
-
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# filename: main.py
 import web
-import datetime
- 
-urls=(
-        '/', 'index',
+from handle import Handle
+
+urls = (
+    '/wx', 'Handle',
 )
- 
-app=web.application(urls, globals())
- 
-class index:
-        def GET(self):
-                return "Hello, world! now is:"+str(datetime.datetime.utcnow())
- 
-if __name__=="__main__":
-        web.wsgi.runwsgi=lambda func,addr=None: web.wsgi.runfcgi(func,addr)
-        app.run()
- 
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()

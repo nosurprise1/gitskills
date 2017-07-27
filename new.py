c42c2fd4b8612658e8333cf968cec5ef1cb1ca3a
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-07-27 10:07:38 +0800

    Update new.py

--- new.py
+++ new.py
@@ -3,9 +3,9 @@ from itchatmp.content import TEXT
 from tuling import get_response
 
 itchatmp.update_config(itchatmp.WechatConfig(
-    token='48fLSTTBDVI5TIDpd1gLdFYwKqex4Mk',
-    appId = 'wx34c7eb36575d6084',
-    appSecret = 'iWPsYqgLW5OWmhUpIXcSlOjwkWjZyGImSJy20F2d9auXmoO6OpSqPXEixgPfJfW5')
+    token='123456',
+    appId = 'wx7860b4c7296dcbdf',
+    appSecret = 'a8db85056d55d3e74d662667b9b015ea')
    
 @itchatmp.msg_register(TEXT)
 def text_reply(msg):

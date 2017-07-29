bcb4a75f3eee9488b72280b05029f000f2a76d3f
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-07-29 19:58:34 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,9 +1,9 @@
 import itchatmp
 
 itchatmp.update_config(itchatmp.WechatConfig(
-    token='yourToken',
-    appId = 'yourAppId',
-    appSecret = 'yourAppSecret'))
+    token='123456',
+    appId = 'wx7860b4c7296dcbdf',
+    appSecret = 'a8db85056d55d3e74d662667b9b015ea'))
 
 @itchatmp.msg_register(itchatmp.content.TEXT)
 def text_reply(msg):

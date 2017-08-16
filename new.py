9cc2d8dbdeadf1a4cf80879abfda77f0d0d877a2
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-08-16 22:06:07 +0800

    Update new.py

--- new.py
+++ new.py
@@ -199,10 +199,12 @@ def text_reply(msg):
     
      
      a=len(piaofen_df)
+     print(a)
      if shoufa==1 and chufa==0 and shoudaifa==0 and chudaifa==0 and shoufufa==0 and chufufa==0 and shoulifa==0 and chulifa==0 and shoucunfa==0 and chucunfa==0:
           for i in range(0,a-1):
               if data.ix[a-1-i,'shou']==1 and (data.ix[a-1-i,'hanglei2']==1 ) and (count<8) and (data.ix[a-1-i,'content'] not in guang)and (friend['NickName'] != data.ix[a-1-i,'nickname'] ):                  
-                  itchatmp.send('%s,%s:%s'%(data.ix[a-1-i,'time2'],data.ix[a-1-i,'nickname'],data.ix[a-1-i,'content']),msg['FromUserName'])
+                  return('%s,%s:%s'%(data.ix[a-1-i,'time2'],data.ix[a-1-i,'nickname'],data.ix[a-1-i,'content']))
+                  #itchatmp.send('%s,%s:%s'%(data.ix[a-1-i,'time2'],data.ix[a-1-i,'nickname'],data.ix[a-1-i,'content']),msg['FromUserName'])
                   guang.append(data.ix[a-1-i,'content'])      
                   count+=1
      elif shoufa==0 and chufa==1 and shoudaifa==0 and chudaifa==0 and shoufufa==0 and chufufa==0 and shoulifa==0 and chulifa==0 and shoucunfa==0 and chucunfa==0:

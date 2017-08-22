c2e9f914d0f774e7cc96f5f8184b1fc5a8bea404
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-08-22 23:08:29 +0800

    Update new3.py

--- new3.py
+++ new3.py
@@ -1,74 +1,5 @@
 # -*- coding: utf-8 -*-
 import itchatmp,json
-import os
-import re
-import time
-import csv
-from itchatmp.content import *
-import pandas as pd
-import numpy as np
-from pandas import DataFrame
-from pymongo import MongoClient
-#client = MongoClient()
-client=MongoClient('mongodb://root:' + '5768116' + '@139.196.79.93')
-#从数据导入piao
-db = client.piao
-collection = db.piao  #http://www.jb51.net/article/77537.htm
-cursor = collection.find()
-piao_df= pd.DataFrame(list(cursor))
-piao_df=piao_df[['xuhao','ci','shou','chu','shoudai','chudai','shouhui','chuhui']]
-piao_df=piao_df.set_index('xuhao')
-piao_df=piao_df.sort_index(ascending=True)
-
-#从数据导入cun
-db = client.cun
-collection = db.cun  #http://www.jb51.net/article/77537.htm
-cursor = collection.find()
-cun_df= pd.DataFrame(list(cursor))
-cun_df=cun_df[['xuhao','ci','shoucun','chucun']]
-cun_df=cun_df.set_index('xuhao')
-cun_df=cun_df.sort_index(ascending=True)
-
-#从数据导入fu
-db = client.fu
-collection = db.fu  #http://www.jb51.net/article/77537.htm
-cursor = collection.find()
-fu_df= pd.DataFrame(list(cursor))
-fu_df=fu_df[['xuhao','ci','shoufu','chufu']]
-fu_df=fu_df.set_index('xuhao')
-fu_df=fu_df.sort_index(ascending=True)
-
-#从数据导入li
-db = client.li
-collection = db.li  #http://www.jb51.net/article/77537.htm
-cursor = collection.find()
-li_df= pd.DataFrame(list(cursor))
-li_df=li_df[['xuhao','ci','shouli','chuli']]
-li_df=li_df.set_index('xuhao')
-li_df=li_df.sort_index(ascending=True)
-
-
-#从数据导入bank
-db2 = client.bank
-collection2 = db2.bank   
-cursor2 = collection2.find()
-bank_df = pd.DataFrame(list(cursor2))
-bank_df=bank_df[['xuhao','yinhang','fenlei1','fenlei2','fenlei3']]
-bank_df=bank_df.set_index('xuhao')
-bank_df=bank_df.sort_index(ascending=True)
-
-    #从数据导入piaofen
-db3 = client.piaofen
-collection3 = db3.piaofen   
-cursor3 = collection3.find()
-piaofen_df = pd.DataFrame(list(cursor3))
-print (piaofen_df)
-    
-    
-
-content=[]
-
-
 
 #连接订阅号
 itchatmp.update_config(itchatmp.WechatConfig(
@@ -79,8 +10,6 @@ itchatmp.update_config(itchatmp.WechatConfig(
 #分析订阅号文本信息
 @itchatmp.msg_register(itchatmp.content.TEXT)
 def text_reply(msg):
-
-
                return msg['content'] 
                
 

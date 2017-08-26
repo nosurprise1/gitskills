b72adc8e286ff2e631b26a851a0c6579579b7a8f
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-08-26 16:28:39 +0800

    Update new5.py

--- new5.py
+++ new5.py
@@ -418,7 +418,7 @@ def text_reply(msg):
                        if piaofen_df.ix[a-1-i,'chucun']==1 and (piaofen_df.ix[a-1-i,'hanglei2']==1 ) and (piaofen_df.ix[a-1-i,'content'] not in guang):                  
                            huifu0=('%s,%s,%s:%s'%(piaofen_df.ix[a-1-i,'time'],piaofen_df.ix[a-1-i,'time2'],piaofen_df.ix[a-1-i,'nickname'],piaofen_df.ix[a-1-i,'content']))
                            huifu=('%s\r\n***************\r\n%s')%(huifu,huifu0)
-                           #itchatmp.send('%s,%s:%s'%(data.ix[a-1-i,'time2'],data.ix[a-1-i,'nickname'],data.ix[a-1-i,'content']),msg['FromUserName'])
+                           
                            guang.append(piaofen_df.ix[a-1-i,'content'])      
                            count+=1
                            if (count==8) or(i>=100):

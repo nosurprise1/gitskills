22a949f8b0cd95324c93369f36f5789fca385190
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-08-27 23:18:51 +0800

    Update new6.py

--- new6.py
+++ new6.py
@@ -59,7 +59,7 @@ cursor3 = collection3.find({"$or":[{'time':'2017-08-25'},{'time':'2017-08-24'}]}
 piaofen_df = pd.DataFrame(list(cursor3))
                   #piaofen_df=piaofen_df.set_index('xuhao')
                   #piaofen_df=piaofen_df.sort_index(ascending=True)
-a=len(piaofen_df)
+
 print(piaofen_df)  
 content=[]
 
@@ -238,6 +238,7 @@ def text_reply(msg):
                   content.append(msg['Content'])  
 
            if hanglei2!=0:
+               
                a=len(piaofen_df)
                if shou==0 and chu==1 and shoudai==0 and chudai==0 and shoufu==0 and chufu==0 and shouli==0 and chuli==0 and shoucun==0 and chucun==0 and shouhui==0 and chuhui==0:
                     for i in range(0,a-1):

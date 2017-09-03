81db220b05fe050cb39d09cd97e52a9de658ad13
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-09-03 09:32:04 +0800

    Update new10.py

--- new10.py
+++ new10.py
@@ -285,18 +285,16 @@ def text_reply(msg):
                    piaofen_df = pd.DataFrame(list(cursor3))
                    print(piaofen_df)
                    a=len(piaofen_df)
-                   for i in range(0,a):
-                       if (piaofen_df.ix[a-1-i,'content'] not in guang):                  
+                   piaofen_df=piaofen_df.drop_duplicates(['content'])
+                   a=len(piaofen_df)
+                   len=min(a,6)
+                   for i in range(0,len):       
                            huifu0=('%s,%s,%s:%s'%(piaofen_df.ix[a-1-i,'time'],piaofen_df.ix[a-1-i,'time2'],piaofen_df.ix[a-1-i,'nickname'],piaofen_df.ix[a-1-i,'content']))
                            #print(huifu0)
                            huifu=('%s\r\n***************\r\n%s')%(huifu,huifu0)
                            #itchatmp.send('%s,%s:%s'%(data.ix[a-1-i,'time2'],data.ix[a-1-i,'nickname'],data.ix[a-1-i,'content']),msg['FromUserName'])
-                           guang.append(piaofen_df.ix[a-1-i,'content'])      
                            count+=1
-                           if (count==6)or (i>=a):
-                               return('111')
-                               print('已发送')
-                               break
+                   return(huifu)
                elif shou==1 and chu==0 and shoudai==0 and chudai==0 and shoufu==0 and chufu==0 and shouli==0 and chuli==0 and shoucun==0 and chucun==0 and shouhui==0 and chuhui==0:
                    shijian11=time.strftime('%y-%m-%d',time.localtime(time.time()))
                    shijian11 = datetime.datetime.strptime(shijian11, "%y-%m-%d")

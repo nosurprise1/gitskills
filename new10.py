b761a337e18a51aa6e675a5fa799064ba4810387
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-09-03 10:17:46 +0800

    Update new10.py

--- new10.py
+++ new10.py
@@ -283,11 +283,12 @@ def text_reply(msg):
                                                    {'hanglei2':1},
                                                     ]})
                    piaofen_df = pd.DataFrame(list(cursor3))
+                   
+                   
                    print(piaofen_df)
                    a=len(piaofen_df)
-                   piaofen_df=piaofen_df.drop_duplicates(['content'])
-                   a=len(piaofen_df)
                    len0=min(a,6)
+                   print(len0)
                    for i in range(0,len0):       
                            huifu0=('%s,%s,%s:%s'%(piaofen_df.ix[a-1-i,'time'],piaofen_df.ix[a-1-i,'time2'],piaofen_df.ix[a-1-i,'nickname'],piaofen_df.ix[a-1-i,'content']))
                            print(huifu0)

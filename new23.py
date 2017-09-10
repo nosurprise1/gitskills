69b930c4fff101c883b5e9a2013aa87b0e01b06e
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-09-10 12:02:02 +0800

    Update new23.py

--- new23.py
+++ new23.py
@@ -138,9 +138,10 @@ def text_reply(msg):
                     #做表
           huatudata3=piaofen_df[['hanglei1','shou','chu','shoudai','chudai','shouhui','chuhui']]
           huatudata4=huatudata3.groupby(['hanglei1']).sum()
-          print(huatudata4)
+          print(str(huatudata4))
           huifu00='机构  收票  出票  收代持  出代持  收回购  出代持'              
           for i in range(0, 2):
+                return(str(huatudata4))
                 huifu0=('中介  %s    %s    %s       %s       %s       %s'%(huatudata4.ix[i,'shou'],huatudata4.ix[i,'chu'],huatudata4.ix[i,'shoudai'],huatudata4.ix[i,'chudai'],huatudata4.ix[i,'shouhui'],huatudata4.ix[i,'chuhui']))
                 print(huifu00)
                 print(huifu0)

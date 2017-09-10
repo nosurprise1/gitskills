6fbde8593f2012fbe342ddf2bf53149f42be75f3
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-09-10 14:30:02 +0800

    Update new23.py

--- new23.py
+++ new23.py
@@ -139,9 +139,10 @@ def text_reply(msg):
           huatudata3=piaofen_df[['hanglei1','shou','chu','shoudai','chudai','shouhui','chuhui']]
           huatudata4=huatudata3.groupby(['hanglei1']).sum()
           huatudata4=huatudata4.reset_index(drop = False)
-    #      print(huatudata4)
+    #     
           huatudata4=huatudata4.rename(columns={'hanglei1': '机构', 'shou': '收', 'chu': '出', 'shoudai': '收代', 'chudai': '出代', 'shouhui':'收代',  'chuhui':'出回'}) 
-         # df.rename(index=str, columns={"A": "a", "C": "c"})
+          huatudata4=huatudata4.reindex('机构') 
+      
 #          huatudata4=DataFrame(huatudata4,columns=['收','出','收代','出代','收回','出回'])
           print(huatudata4)
           return(str(huatudata4))

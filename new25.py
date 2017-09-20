56bf4d118e7ea8b6984d1d6d8914b260402e814c
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-09-20 15:53:32 +0800

    Update new25.py

--- new25.py
+++ new25.py
@@ -267,11 +267,11 @@ def text_reply(msg):
                if string[1] == str(zixun_df.ix[a-1-i,'获取号']):
                    
                    neirong=str(zixun_df.ix[a-1-i,'内容'])[0:400]
-                   neirong= neirong.replace('  ','\n')
+                  # neirong= neirong.replace('  ','\n')
                    laiyuan=zixun_df.ix[a-1-i,'序号']
                    biaoti=zixun_df.ix[a-1-i,'标题']
                    shijian=zixun_df.ix[a-1-i,'时间']
-                   huiful=('%s,《%s》,来源“%s”：%s……\n%s'%(shijian,biaoti,laiyuan,neirong,zixun_df.ix[a-1-i,'链接']))
+                   huiful=('%s,《%s》,来源“%s”：\n%s……\n%s'%(shijian,biaoti,laiyuan,neirong,zixun_df.ix[a-1-i,'链接']))
                    #return('zhidaol ')
                    return (huiful)
                    

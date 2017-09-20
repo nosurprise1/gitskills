516aa9ab7bb179acdb7e1b606edcb726746c7dca
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-09-20 15:51:28 +0800

    Update new25.py

--- new25.py
+++ new25.py
@@ -267,7 +267,7 @@ def text_reply(msg):
                if string[1] == str(zixun_df.ix[a-1-i,'获取号']):
                    
                    neirong=str(zixun_df.ix[a-1-i,'内容'])[0:400]
-                   neirong= neirong.replace('  ','\n  ')
+                   neirong= neirong.replace('  ','\n')
                    laiyuan=zixun_df.ix[a-1-i,'序号']
                    biaoti=zixun_df.ix[a-1-i,'标题']
                    shijian=zixun_df.ix[a-1-i,'时间']

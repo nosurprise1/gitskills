4d0b4ce637bee9d6f85ce711c520252a7e5ebb9d
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-09-27 13:15:39 +0800

    Update new25.py

--- new25.py
+++ new25.py
@@ -7,25 +7,8 @@ from pandas import DataFrame
 from pymongo import MongoClient
 client=MongoClient('mongodb://root:' + '5768116' + '@139.196.79.93')
 
-#333global content,collection3,piaofen_df,shijian11,shijian0,shijian01,shijian02
-shijian11=time.strftime('%y-%m-%d',time.localtime(time.time()))
-shijian11 = datetime.datetime.strptime(shijian11, "%y-%m-%d")
-shijian10=shijian11-datetime.timedelta(days=1)  #明天
-
-shijian0=shijian11-datetime.timedelta(days=1)
-shijian01=shijian11-datetime.timedelta(days=2)
-shijian02=shijian11-datetime.timedelta(days=3)
-shijian11=shijian11.strftime("%Y-%m-%d")  #今天
-shijian0=shijian0.strftime("%Y-%m-%d")     #昨天
-shijian01=shijian01.strftime("%Y-%m-%d")   #前天
-shijian02=shijian02.strftime("%Y-%m-%d")   #大前天
 
 
-
-print(shijian11)
-print(shijian0)
-print(shijian01)
-print(shijian02)
 
 
 #从数据导入piao
@@ -82,6 +65,11 @@ collection3 = db3.piaofen
 #piaofen_df = pd.DataFrame(list(cursor3))
 content=[]
 
+db4 = client.cundanfenxi
+collection4 = db4.cundanfenxi 
+
+
+
 #连接订阅号
 itchatmp.update_config(itchatmp.WechatConfig(
     token='123456',
@@ -92,7 +80,7 @@ itchatmp.update_config(itchatmp.WechatConfig(
 #分析订阅号文本信息
 @itchatmp.msg_register(itchatmp.content.TEXT)
 def text_reply(msg):
-     global content,collection3,piaofen_df,shijian11,shijian0,shijian01,shijian02,zixun_df
+     global content,collection3,collection4,piaofen_df,shijian11,shijian10,shijian0,shijian01,shijian02,zixun_df
      guang=[]
      count=0
      shijian1=time.strftime('%Y-%m-%d',time.localtime(time.time()))
@@ -335,7 +323,23 @@ def text_reply(msg):
           huatudata4=huatudata4.set_index('机构')
           print(huatudata4)
           return(str(huatudata4)) 
-       elif string[0]=='存单发行':
+       elif string[0]=='存单发行0':
+           shijian11=time.strftime('%y-%m-%d',time.localtime(time.time()))
+           shijian11 = datetime.datetime.strptime(shijian11, "%y-%m-%d")
+           shijian10=shijian11-datetime.timedelta(days=1)  #明天
+
+           shijian0=-datetime.timedelta(days=1)
+           shijian01=shijian11-datetime.timedelta(days=2)
+           shijian02=shijian11-datetime.timedelta(days=3)
+           shijian11=shijian11.strftime("%Y-%m-%d")  #今天
+           shijian0=shijian0.strftime("%Y-%m-%d")     #昨天
+           shijian01=shijian01.strftime("%Y-%m-%d")   #前天
+           shijian02=shijian02.strftime("%Y-%m-%d")   #大前天
+           print(shijian11)
+           print(shijian0)
+           print(shijian01)
+           print(shijian02)
+         
            db = client.cundan
            collection = db.cundan  
            cursor = collection.find({ "$and":[{"$or":[{'发行日':shijian11},{'发行日':shijian10},{'发行日':shijian0}]},  {'类型':'同业存单'}   ] })  
@@ -360,7 +364,9 @@ def text_reply(msg):
            biao0['计划加权利率']=biao0['ji2']/biao0['计划发行']
            biao0=biao0[['实际发行','计划发行','实际加权利率','计划加权利率']]
            biao0=biao0.round({'实际发行':2,'计划发行':2,'实际加权利率':2,'计划加权利率':2})
-           return('1')
+           records = json.loads(biao0.T.to_json()).values()
+           collection10.insert(records)
+           return('分析成功')
         
        elif string[0]=='理财分析':
           shijian2=time.strftime('%Y-%m-%d',time.localtime(time.time()))

a9d9cef3ea6f276899aa3934a1c619ad7d4cb5d7
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-11-21 16:58:32 +0800

    Update new31.py

--- new31.py
+++ new31.py
@@ -212,7 +212,7 @@ def text_reply(msg):
           huatudata3=piaofen_df[['hanglei1','shou','chu','shoudai','chudai','shouhui','chuhui']]
           huatudata4=huatudata3.groupby(['hanglei1']).sum()
           huatudata4=huatudata4.reset_index(drop = False)
-          huatudata4=huatudata4.rename(columns={'hanglei1': '机构', 'shou': '收', 'chu': '出', 'shoudai': '收代', 'chudai': '出代'}) 
+          huatudata4=huatudata4.rename(columns={'hanglei1': '机构', 'shou': '收', 'chu': '出', 'shoudai': '收代持', 'chudai': '出代持'}) 
           #huatudata4=huatudata4.set_index('机构')
           #df2 = df2.reset_index(drop=True)    #重新定义索引
           huatuhui='以下未即时广告计数（已排除重复广告）'

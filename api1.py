6a223eeda506dac72c05a6b78e92645e79b415fe
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-12-19 13:35:45 +0800

    Update api1.py

--- api1.py
+++ api1.py
@@ -8,23 +8,6 @@ auth = HTTPBasicAuth()
 
 app = Flask(__name__)
 client=MongoClient('mongodb://root:' + '5768116' + '@139.196.79.93')
-#shijian2=time.strftime('%Y-%m-%d',time.localtime(time.time()))
-#shijian2 = datetime.datetime.strptime(shijian2, "%Y-%m-%d")
-#shijian2=shijian2.strftime("%Y-%m-%d")  #今天
-         
-#db3 = client.piaofen
-#collection3 = db3.piaofen   
-#cursor3 = collection3.find({
-#                                                'time':str(shijian2)
-#                                                 })
-#print(type(cursor3))
-
-#piaofen_df = pd.DataFrame(list(cursor3))
-
-#print(piaofen_df.encode("utf-8", "ignore") )
-#piaofen=piaofen_df.to_json(orient='split')
-#print(piaofen)
-
 
 
 

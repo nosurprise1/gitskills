b130fffb9d79c495fb22f24b5eeae70dcb19934a
nosurprise1 <30318760+nosurprise1@users.noreply.github.com>
2017-12-22 10:45:19 +0800

    Update api1.py

--- api1.py
+++ api1.py
@@ -243,7 +243,7 @@ def get_tasks(task_id):
                    piaofen_df=piaofen_df.to_json()
 
                    return(piaofen_df)                
-    elif task_id=14:
+    elif task_id==14:
           return(sou)                     
     else:
         return('网络不好!!!!!')
